c8f2a003a32f0403ef4df685e7eefa7640ad4f64
maton369 <ysk901085@gmail.com>
2025-05-23 09:12:44 +0900

    Initial Commit

---
+++ ProgrammingStartTests/ProgrammingStartTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammingStartTests.swift
+//  ProgrammingStartTests
+//
+//  Created by 清水悠介 on 2025/05/23.
+//
+
+import Testing
+@testable import ProgrammingStart
+
+struct ProgrammingStartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
