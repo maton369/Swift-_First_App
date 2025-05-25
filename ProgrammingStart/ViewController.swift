664fa34f8d812ed6aa0bbdf07d3cb780992753f9
maton369 <ysk901085@gmail.com>
2025-05-25 12:09:37 +0900

    Add: CGRect

--- ProgrammingStart/ViewController.swift
+++ ProgrammingStart/ViewController.swift
@@ -11,8 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello World!")
+        
+        let uiView=UIView()
+        uiView.frame=CGRect(x: 50, y: 100, width: 100, height: 100)
+        uiView.backgroundColor=UIColor.systemBlue
+        view.addSubview(uiView)
     }
 
 
