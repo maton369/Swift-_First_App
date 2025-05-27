3c8e5fa7e90968bf76b562ff717bd1ccc47df573
maton369 <ysk901085@gmail.com>
2025-05-27 09:42:56 +0900

    Add: ブルーの図形を親とし、シアンの図形を配置

--- ProgrammingStart/ViewController.swift
+++ ProgrammingStart/ViewController.swift
@@ -2,7 +2,6 @@
 //  ViewController.swift
 //  ProgrammingStart
 //
-//  Created by 清水悠介 on 2025/05/23.
 //
 
 import UIKit
@@ -12,10 +11,19 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     let size = view.frame.size
+
+    // ブルーのビュー（親ビュー）
     let uiView = UIView()
-    uiView.frame.size = CGSize(width: 100, height: 100)
+    uiView.frame.size = CGSize(width: 200, height: 200)
     uiView.center = CGPoint(x: size.width / 2, y: size.height / 2)
     uiView.backgroundColor = UIColor.systemBlue
     view.addSubview(uiView)
+
+    // シアンのビュー（子ビューとしてブルーの中に追加）
+    let uiView2 = UIView()
+    uiView2.frame.size = CGSize(width: 150, height: 150)
+    uiView2.center = CGPoint(x: uiView.bounds.width / 2, y: uiView.bounds.height / 2)
+    uiView2.backgroundColor = UIColor.cyan
+    uiView.addSubview(uiView2) // ここが重要：親ビューに追加
   }
 }
