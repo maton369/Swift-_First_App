b7d17612ec479fc086535fbefa8e49615ba7c0fa
maton369 <ysk901085@gmail.com>
2025-05-29 09:02:58 +0900

    Add: Color

--- ProgrammingStart/Colors.swift
+++ ProgrammingStart/Colors.swift
@@ -1,8 +1,18 @@
-//
-//  Colors.swift
-//  ProgrammingStart
-//
-//  Created by 清水悠介 on 2025/05/28.
-//
+import UIKit
 
-import Foundation
+struct Colors {
+  let bluePurple = UIColor(red: 87 / 255, green: 99 / 255, blue: 175 / 255, alpha: 1)
+  let blue = UIColor(red: 92 / 255, green: 137 / 255, blue: 200 / 255, alpha: 1)
+  let blueGreen = UIColor(red: 76 / 255, green: 175 / 255, blue: 167 / 255, alpha: 1)
+  let green = UIColor(red: 160 / 255, green: 194 / 255, blue: 95 / 255, alpha: 1)
+  let yellowGreen = UIColor(red: 175 / 255, green: 215 / 255, blue: 115 / 255, alpha: 1)
+  let yellow = UIColor(red: 251 / 255, green: 245 / 255, blue: 135 / 255, alpha: 1)
+  let yellowOrange = UIColor(red: 254 / 255, green: 220 / 255, blue: 120 / 255, alpha: 1)
+  let orange = UIColor(red: 246 / 255, green: 169 / 255, blue: 115 / 255, alpha: 1)
+  let redOrange = UIColor(red: 245 / 255, green: 141 / 255, blue: 116 / 255, alpha: 1)
+  let red = UIColor(red: 245 / 255, green: 121 / 255, blue: 105 / 255, alpha: 1)
+  let redPurple = UIColor(red: 206 / 255, green: 126 / 255, blue: 181 / 255, alpha: 1)
+  let purple = UIColor(red: 129 / 255, green: 107 / 255, blue: 177 / 255, alpha: 1)
+  let white = UIColor.systemBackground
+  let black = UIColor(red: 0, green: 0, blue: 0, alpha: 0.9)
+}
